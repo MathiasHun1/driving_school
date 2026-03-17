8a1aa68c3b11e250b02535b8b49585a5d1fd3244
MathiasHun1 <141583942+MathiasHun1@users.noreply.github.com>
2026-03-17 20:31:53 +0100

    enable scroll position restoration with in-memory scrolling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
 ﻿import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import {provideRouter, withInMemoryScrolling} from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+      })),
     provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
   ],
